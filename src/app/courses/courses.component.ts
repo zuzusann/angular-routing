955484286e708b75a581b6fa856957c33d720aea
zuzusann <zuzusan65@gmail.com>
2025-02-05 20:58:23 +0630

    [MOD] update search btn in banner component using query string

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,13 +1,41 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Course } from '../Models/course';
 import { CourseService } from '../Services/course.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
   coursesService = inject(CourseService);
-  AllCourses: Course[] = this.coursesService.courses;
+  AllCourses: Course[];
+  searchString: string;
+
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+      // this.searchString =  this.activeRoute.snapshot.queryParams['search'];
+      // this.searchString = this.activeRoute.snapshot.queryParamMap.get('search');
+     
+      // this.activeRoute.queryParams.subscribe((data) => {
+      //   this.searchString = data['search'];
+      // })
+
+      this.activeRoute.queryParamMap.subscribe((data) => {
+        this.searchString = data.get('search');
+
+        if(this.searchString === undefined || this.searchString === '' || this.searchString === null){
+          this.AllCourses = this.coursesService.courses
+        }else{
+          this.AllCourses = this.coursesService.courses
+            .filter(x => x.title.toLowerCase()
+          .includes(this.searchString.toLowerCase()));
+        }
+      })
+      
+  }
+
+  
 }
--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent {
+
+  router: Router = inject(Router);
+
+  onSearchClicked(value: string){
+    this.router.navigate(['/Courses'], {queryParams: {search: value}})
+  }
 
 }
