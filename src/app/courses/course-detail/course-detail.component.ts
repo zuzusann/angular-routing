de97d5dfe2e09cebb548ab980b677b2df5e1fe8c
zuzusann <zuzusan65@gmail.com>
2025-02-05 16:36:16 +0630

    Configure basic routes for Home, Courses, and About components

---
+++ src/app/courses/course-detail/course-detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from 'src/app/Models/course';
+import { CourseService } from 'src/app/Services/course.service';
+
+@Component({
+  selector: 'app-course-detail',
+  templateUrl: './course-detail.component.html',
+  styleUrls: ['./course-detail.component.css']
+})
+export class CourseDetailComponent implements OnInit, OnDestroy{
+
+  selectedCourse: Course;
+  courseId: number;
+
+  courseService: CourseService = inject(CourseService);
+
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  paramMapObs;
+
+  ngOnInit(): void {
+      // this.courseId = this.activeRoute.snapshot.params['id'];
+      // this.courseId = +this.activeRoute.snapshot.paramMap.get('id');
+      // this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId);
+
+      // this.activeRoute.params.subscribe((data) => {
+      //   this.courseId = +data['id']
+      //   this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId)
+      // })
+
+      this.paramMapObs = this.activeRoute.paramMap.subscribe((data) => {
+        this.courseId = +data.get('id');
+        this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId);
+      })
+  }
+
+  ngOnDestroy(): void {
+      this.paramMapObs.unsubscribe();
+  }
+}
